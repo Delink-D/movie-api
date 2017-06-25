a52bf7d12909615c88266b50f6eddb914c68ad23
Delink-D <delinkdesigns@gmail.com>
2017-06-26 00:58:03 +0300

    Alter of moviesController
    Movies respose in form of an object
    Connect to the databse, mongodb from the app.js file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // require express a node framework
 var express = require('express');
+var mongoose = require('mongoose');
 var movieRouter = require('./router/moviesRouter');
 var app = express();
+
+// a database connection to the mongodb
+var db = mongoose.connect("mongodb://localhost/movies");
 
 // when this url with /movies is called use movieRouter function
 app.use('/movies', movieRouter);
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,6 +1,17 @@
+// require the model
+var movie = require('./../models/moviesModels');
+
 var get = function(req, res) {
 	// the response to be sent when this request is made
-	res.send('List of movies!');
+	movie.find(function(err, movies) {
+		if (err) {
+			res.status(500)
+			res.send('Internal server Error!');
+		}else{
+			res.status(200)
+			res.send(movies);
+		}
+	})
 };
 
 module.exports = get;
