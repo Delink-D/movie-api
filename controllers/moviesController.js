fa30ed8e58aaa449386304b61da7ab360ea4cfbc
Delink-D <delinkdesigns@gmail.com>
2017-06-26 01:12:13 +0300

    A controller function to add movies
    A function to all new movie to the database

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,17 +1,37 @@
 // require the model
-var movie = require('./../models/moviesModels');
+var Movie = require('./../models/moviesModels');
 
+// fnction to get movies
 var get = function(req, res) {
 	// the response to be sent when this request is made
-	movie.find(function(err, movies) {
+	Movie.find(function(err, movie) {
 		if (err) {
-			res.status(500)
+			res.status(500);
 			res.send('Internal server Error!');
 		}else{
-			res.status(200)
-			res.send(movies);
+			res.status(200);
+			res.send(movie);
 		}
 	})
-};
+}
 
-module.exports = get;
+// function to save movies to db
+var add = function(req, res) {
+	var movie = new Movie(req, body);
+	movie.save(function(err) {
+		if (err) {
+			res.status(500);
+			res.send('Error while saving, FAILED!');
+		}else{
+			res.status(201);
+			res.send(movie)
+		}
+	});
+}
+
+// module.exports = get;
+// module.exports = add;
+module.exports = {
+	get: get,
+	add: add
+}
