2dac8355a4a2b534a317e73b850236f8cfa43b57
Delink-D <delinkdesigns@gmail.com>
2017-06-25 23:46:43 +0300

    Setting up of router
    Function to be called when a /movies url is requested
    Making good use of comments in code

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// require express a node framework
 var express = require('express');
-
 var app = express();
 
+// setting up of router
+/*
+	List of movies route starts
+*/
+var movieRouter = express.Router();
+movieRouter.route('')
+	.get(function(req, res) {
+	// the response to be sent when this request is made
+	res.send('List of movies!');
+});
 
+// when this url with /movies is called use movieRouter function
+app.use('/movies', movieRouter);
+/*
+	./ List of movies route ends
+*/
+
+// setting of app to listen to port: 3000
 app.listen(3000, function() {
 	console.log("App listening to port: 3000");
 });
